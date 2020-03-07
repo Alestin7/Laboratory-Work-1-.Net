ae449de719c211af9c691f8124c0b02fff7d98f1
Алексей Костин <kostinalesha837@gmail.com>
2020-03-07 18:11:50 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Laboratory_Work_1_.Net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите переменную a: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int result = (int)Math.Pow(a, 2);
+            Console.WriteLine($"Квадрат переменной a = {result}");
+        }
+    }
+}
